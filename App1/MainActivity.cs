5369cf1e825e93e50a471f791d8dc26410b51675
PC-ASUS-ROG <mathieusoufflard@gmail.com>
2020-05-29 16:12:56 +0200

    Update .suo, db.lock, and 1496 more files...

---
+++ App1/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            System.Threading.Timer tim = new System.Threading.Timer(obj => OnCallBack(), null, 0, 1000);
+        }
+
+        private void OnCallBack()
+        {
+            TextView tv = FindViewById<TextView>(Resource.Id.time);
+            tv.Text = System.DateTime.UtcNow.ToString("F");
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
